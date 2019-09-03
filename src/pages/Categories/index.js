8abd27d9d5d254243eb8fbe24d60f993af9c3fa5
Rafael <rafaelbpa@gmail.com>
2019-09-02 23:31:37 -0300

    Refactoring component

--- src/pages/Categories/index.js
+++ src/pages/Categories/index.js
@@ -1,13 +1,26 @@
 import React from 'react';
 
-// import { Container } from './styles';
+import { Container, Content, Title, CategoryList } from './styles';
 
 import Header from '~/components/Header';
+import CategoryCard from '~/components/CategoryCard';
 
 export default function Categories() {
   return (
-    <div>
+    <Container>
       <Header />
-    </div>
+      <Content>
+        <Title>Categorias</Title>
+        <CategoryList>
+          <CategoryCard category="Mitologia" />
+          <CategoryCard category="Mitologia" />
+          <CategoryCard category="Mitologia" />
+          <CategoryCard category="Mitologia" />
+          <CategoryCard category="Mitologia" />
+          <CategoryCard category="Mitologia" />
+          <CategoryCard category="Mitologia" />
+        </CategoryList>
+      </Content>
+    </Container>
   );
 }
--- src/pages/Categories/styles.js
+++ src/pages/Categories/styles.js
@@ -1,3 +1,40 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  height: 100%;
+  background: #e5e5e5;
+`;
+
+export const Content = styled.div`
+  max-width: 1060px;
+  margin: 0 auto;
+  display: flex;
+  flex: 1;
+  flex-direction: column;
+`;
+
+export const Title = styled.span`
+  font-size: 44px;
+  line-height: 56px;
+  color: #1e2124;
+  margin-top: 48px;
+  margin-bottom: 32px;
+`;
+
+export const CategoryList = styled.ul`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  align-items: flex-start;
+
+  li {
+    margin-right: 24px;
+    margin-bottom: 25px;
+    flex-grow: 1;
+    flex-shrink: 1;
+
+    :last-child {
+      /* margin-right: 0; */
+    }
+  }
+`;
